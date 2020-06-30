7269afd10a10835e307db8fcb33c25f2335fe670
Andrea Zakovski <andreazakovski@Andreas-MacBook-Pro.local>
2020-06-30 17:41:45 +0200

    header and main slider added on homepage

---
+++ app.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  // loop: true,
+  direction: "vertical",
+  autoplay: {
+    delay: 2500,
+  },
+  speed: 800,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: false,
+  },
+});
