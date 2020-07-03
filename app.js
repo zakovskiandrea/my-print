c500f4d78e562a83848135807f6c5694af16fc75
Andrea Zakovski <andreazakovski@Andreas-MacBook-Pro.local>
2020-07-03 11:24:06 +0200

    similar-products section added

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// var swiper = new Swiper(".swiper-container--homepage", {
-//   slidesPerView: 1,
-//   // loop: true,
-//   direction: "vertical",
-//   autoplay: {
-//     delay: 5000,
-//   },
-//   speed: 1000,
-//   scrollbar: {
-//     el: ".swiper-scrollbar",
-//     hide: false,
-//   },
-// });
+var swiper = new Swiper(".swiper-container--homepage", {
+  slidesPerView: 1,
+  // loop: true,
+  direction: "vertical",
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 1000,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: false,
+  },
+});
 
 var swiper = new Swiper(".swiper-container--eshop", {
   loop: true,
@@ -29,6 +29,16 @@ var swiper = new Swiper(".swiper-container--products", {
   navigation: {
     nextEl: ".homepage-products__slider-box .arrow--next",
     prevEl: ".homepage-products__slider-box .arrow--prev",
+  },
+});
+
+var swiper = new Swiper(".swiper-container--similar-products", {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".similar-products__slider-box .arrow--next",
+    prevEl: ".similar-products__slider-box .arrow--prev",
   },
 });
 
