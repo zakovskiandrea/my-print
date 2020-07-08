03dac27b0419a089852961a904c3e80ab4bb6969
Andrea Zakovski <andreazakovski@Andreas-MacBook-Pro.local>
2020-07-08 14:13:22 +0200

    brandig section added on homepage

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// var swiper = new Swiper(".swiper-container--homepage", {
-//   slidesPerView: 1,
-//   // loop: true,
-//   direction: "vertical",
-//   autoplay: {
-//     delay: 5000,
-//   },
-//   speed: 1000,
-//   scrollbar: {
-//     el: ".swiper-scrollbar",
-//     hide: false,
-//   },
-// });
+var swiper = new Swiper(".swiper-container--homepage", {
+  slidesPerView: 1,
+  // loop: true,
+  direction: "vertical",
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 1000,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: false,
+  },
+});
 
 var swiper = new Swiper(".swiper-container--eshop", {
   loop: true,
@@ -109,4 +109,44 @@ $(".header-phone__dropbtn").click((e) => {
 
 $(document).click(function () {
   $(".header-phone__dropdown-content").removeClass("show");
+});
+
+gsap.to(".homepage-branding", {
+  scrollTrigger: {
+    start: "top +=100",
+    pin: ".homepage-branding__img",
+    endTrigger: ".container-right",
+    end: "bottom +=100%",
+  },
+});
+
+let sections = gsap.utils.toArray(".homepage-branding__content");
+
+sections.forEach((element) => {
+  gsap.to(".homepage-branding__content", {
+    scrollTrigger: {
+      trigger: element,
+      start: "top center",
+      end: "bottom",
+      // markers: true,
+      onEnter: (progress) => {
+        document
+          .querySelectorAll(".branding-img")
+          .forEach((img) => (img.style.opacity = 0));
+        console.log(progress.trigger.id);
+        document.querySelector("." + progress.trigger.id).style.opacity = 1;
+      },
+      onLeaveBack: (progress) => {
+        const contentString = progress.trigger.id;
+        const num = contentString.slice(-1) * 1 - 1;
+        if (num === 0) return;
+        const newContentString =
+          contentString.substring(0, contentString.length - 1) + num;
+        document
+          .querySelectorAll(".branding-img")
+          .forEach((img) => (img.style.opacity = 0));
+        document.querySelector("." + newContentString).style.opacity = 1;
+      },
+    },
+  });
 });
