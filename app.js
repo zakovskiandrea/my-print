54f469c4dc7e98ece29a2e3937e7c2bc96fb81b1
Andrea Zakovski <andreazakovski@Andreas-MacBook-Pro.local>
2020-07-15 14:48:32 +0200

    gallery modal added on products pages

--- app.js
+++ app.js
@@ -231,3 +231,30 @@ $(document).ready(function () {
     } // End if
   });
 });
+
+$("#exampleModal").on("show.bs.modal", function (e) {
+  console.log("modal opened");
+  setTimeout(() => {
+    var galleryThumbs = new Swiper(".gallery-thumbs--modal", {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+    });
+    var galleryTop = new Swiper(".gallery-top--modal", {
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      thumbs: {
+        swiper: galleryThumbs,
+      },
+    });
+  }, 500);
+});
+
+// $(".characteristics__btn").click(() => {
+//   $(".modal-dialog").modal("show");
+// });
